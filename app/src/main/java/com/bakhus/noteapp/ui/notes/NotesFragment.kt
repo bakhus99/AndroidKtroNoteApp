d415a4aad23c6c0b1f4c267fb52c8fffb7a2856a
baha <tytenov99@gmail.com>
2021-05-22 20:10:45 +0600

    added showing notes

--- app/src/main/java/com/bakhus/noteapp/ui/notes/NotesFragment.kt
+++ app/src/main/java/com/bakhus/noteapp/ui/notes/NotesFragment.kt
@@ -1,18 +1,25 @@
 package com.bakhus.noteapp.ui.notes
 
 import android.content.SharedPreferences
+import android.content.pm.ActivityInfo.SCREEN_ORIENTATION_USER
 import android.os.Bundle
 import android.view.*
 import android.viewbinding.library.fragment.viewBinding
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bakhus.noteapp.R
+import com.bakhus.noteapp.adapters.NoteAdapter
 import com.bakhus.noteapp.databinding.FragmentNotesBinding
 import com.bakhus.noteapp.ui.BaseFragment
 import com.bakhus.noteapp.utils.Constants.KEY_LOGGED_IN_EMAIL
 import com.bakhus.noteapp.utils.Constants.KEY_PASSWORD
 import com.bakhus.noteapp.utils.Constants.NO_EMAIL
 import com.bakhus.noteapp.utils.Constants.NO_PASSWORD
+import com.bakhus.noteapp.utils.Status
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -21,6 +28,9 @@ class NotesFragment() : BaseFragment(R.layout.fragment_notes) {
 
 
     private val binding: FragmentNotesBinding by viewBinding()
+    private lateinit var noteAdapter: NoteAdapter
+
+    private val viewModel: NotesViewModel by viewModels()
 
     @Inject
     lateinit var sharedPref: SharedPreferences
@@ -38,6 +48,57 @@ class NotesFragment() : BaseFragment(R.layout.fragment_notes) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        requireActivity().requestedOrientation = SCREEN_ORIENTATION_USER
+        setupRecyclerView()
+        subscribeToObservers()
+        noteAdapter.setOnItemClickListener {
+            val acton = NotesFragmentDirections.actionNotesFragmentToNoteDetailFragment(it.id)
+            findNavController().navigate(acton)
+        }
+        binding.fabAddNote.setOnClickListener {
+            val action = NotesFragmentDirections.actionNotesFragmentToAddEditNoteFragment("")
+            findNavController().navigate(action)
+        }
+    }
+
+
+    private fun subscribeToObservers() {
+        viewModel.allNotes.observe(viewLifecycleOwner, Observer {
+            it?.let { event ->
+                val result = event.peekContent()
+                when (result.status) {
+                    Status.SUCCESS -> {
+                        noteAdapter.notes = result.data!!
+                        binding.swipeRefreshLayout.isRefreshing = false
+                    }
+                    Status.ERROR -> {
+                        event.getContentIfNotHandled()?.let { errorResoure ->
+                            errorResoure.message?.let { msg ->
+                                Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                        result.data?.let { notes ->
+                            noteAdapter.notes = notes
+                        }
+                        binding.swipeRefreshLayout.isRefreshing = false
+
+                    }
+                    Status.LOADING -> {
+                        result.data?.let { notes ->
+                            noteAdapter.notes = notes
+                        }
+                        binding.swipeRefreshLayout.isRefreshing = true
+                    }
+                }
+            }
+        })
+    }
+
+    private fun setupRecyclerView() = binding.rvNotes.apply {
+
+        noteAdapter = NoteAdapter()
+        adapter = noteAdapter
+        layoutManager = LinearLayoutManager(requireContext())
 
     }
 
