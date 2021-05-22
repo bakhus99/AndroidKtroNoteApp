388aaff4df1aad42150dc62bc286564bfca64a52
baha <tytenov99@gmail.com>
2021-05-22 11:34:57 +0600

    added di

---
+++ app/src/main/java/com/bakhus/noteapp/NoteApplication.kt
@@ -0,0 +1,15 @@
+package com.bakhus.noteapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class NoteApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+
+}
--- app/src/main/java/com/bakhus/noteapp/data/local/NotesDatabase.kt
+++ app/src/main/java/com/bakhus/noteapp/data/local/NotesDatabase.kt
@@ -1,6 +1,7 @@
 package com.bakhus.noteapp.data.local
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.bakhus.noteapp.data.local.entites.Note
 
@@ -9,7 +10,7 @@ import com.bakhus.noteapp.data.local.entites.Note
     entities = [Note::class],
     version = 1
 )
-abstract class NotesDatabase {
+abstract class NotesDatabase : RoomDatabase() {
 
     @TypeConverters(Converters::class)
     abstract fun noteDao(): NoteDao
