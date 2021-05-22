c2b43fb6811b49acaf65932cd59c26363ae8f738
baha <tytenov99@gmail.com>
2021-05-22 14:25:43 +0600

    added login user

--- app/src/main/java/com/bakhus/noteapp/ui/auth/AuthFragment.kt
+++ app/src/main/java/com/bakhus/noteapp/ui/auth/AuthFragment.kt
@@ -1,5 +1,6 @@
 package com.bakhus.noteapp.ui.auth
 
+import android.content.SharedPreferences
 import android.content.pm.ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 import android.os.Bundle
 import android.view.View
@@ -8,18 +9,31 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
 import com.bakhus.noteapp.R
+import com.bakhus.noteapp.data.remote.BasicAuthInterceptor
 import com.bakhus.noteapp.databinding.FragmentAuthBinding
-import com.bakhus.noteapp.ui.BaseFragment
+import com.bakhus.noteapp.utils.Constants.KEY_LOGGED_IN_EMAIL
+import com.bakhus.noteapp.utils.Constants.KEY_PASSWORD
 import com.bakhus.noteapp.utils.Status
-import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class AuthFragment() : Fragment(R.layout.fragment_auth) {
 
     private val viewModel: AuthViewModel by viewModels()
     private val binding: FragmentAuthBinding by viewBinding()
+
+    @Inject
+    lateinit var sharedPref: SharedPreferences
+
+    @Inject
+    lateinit var basicAuthInterceptor: BasicAuthInterceptor
+
+    private var curEmail: String? = null
+    private var curPassword: String? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -35,17 +49,75 @@ class AuthFragment() : Fragment(R.layout.fragment_auth) {
             viewModel.register(email, password, confirmedPassword)
 
         }
-
+        binding.btnLogin.setOnClickListener {
+            val email = binding.etLoginEmail.text.toString()
+            val password = binding.etLoginPassword.text.toString()
+            curEmail = email
+            curPassword = password
+            viewModel.login(email, password)
+        }
 
     }
 
+    private fun authenticateApi(email: String, password: String) {
+        basicAuthInterceptor.email = email
+        basicAuthInterceptor.password = password
+
+    }
+
+    private fun redirectLogin() {
+        val navOptions = NavOptions.Builder()
+            .setPopUpTo(R.id.authFragment, true)
+            .build()
+        findNavController().navigate(
+            AuthFragmentDirections.actionAuthFragmentToNotesFragment(),
+            navOptions
+        )
+    }
+
+
     private fun subscribeToObservers() {
+        viewModel.loginStatus.observe(viewLifecycleOwner, Observer { result ->
+            result?.let {
+                when (result.status) {
+                    Status.SUCCESS -> {
+                        binding.loginProgressBar.visibility = View.GONE
+                        Toast.makeText(
+                            requireContext(),
+                            result.data ?: "Successfully logged in",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        sharedPref.edit().putString(KEY_LOGGED_IN_EMAIL, curEmail).apply()
+                        sharedPref.edit().putString(KEY_PASSWORD, curPassword).apply()
+                        authenticateApi(curEmail ?: "", curPassword ?: "")
+                        redirectLogin()
+                    }
+                    Status.ERROR -> {
+                        binding.loginProgressBar.visibility = View.GONE
+                        Toast.makeText(
+                            requireContext(),
+                            result.message ?: "An unknown error occurred",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                    }
+                    Status.LOADING -> {
+                        binding.loginProgressBar.visibility = View.VISIBLE
+                    }
+                }
+            }
+        })
+
         viewModel.registerStatus.observe(viewLifecycleOwner, Observer { result ->
             result?.let {
                 when (result.status) {
                     Status.SUCCESS -> {
                         binding.registerProgressBar.visibility = View.GONE
-                        Toast.makeText(requireContext(), result.data ?: "successfully registered account", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            requireContext(),
+                            result.data ?: "successfully registered account",
+                            Toast.LENGTH_SHORT
+                        ).show()
                         //Snackbar.make(requireContext(),(result.data ?: "successfully registered account"),Snackbar.LENGTH_SHORT)
 
                     }
