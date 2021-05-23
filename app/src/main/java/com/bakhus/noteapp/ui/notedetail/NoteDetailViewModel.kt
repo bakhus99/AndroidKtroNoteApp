92799d8e527d9658e3893692f373f5945043c40e
baha <tytenov99@gmail.com>
2021-05-23 16:59:49 +0600

    added formating notes by markdown

--- app/src/main/java/com/bakhus/noteapp/ui/notedetail/NoteDetailViewModel.kt
+++ app/src/main/java/com/bakhus/noteapp/ui/notedetail/NoteDetailViewModel.kt
@@ -1,6 +1,15 @@
 package com.bakhus.noteapp.ui.notedetail
 
 import androidx.lifecycle.ViewModel
+import com.bakhus.noteapp.repository.NoteRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class NoteDetailViewModel: ViewModel() {
+@HiltViewModel
+class NoteDetailViewModel @Inject constructor(
+    private val repository: NoteRepository
+) : ViewModel() {
+
+    fun observeNoteByID(noteID:String) = repository.observeNoteByID(noteID)
+
 }
